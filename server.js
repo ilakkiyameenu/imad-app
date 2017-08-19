2e15de5cadf0503f985076912e14616e9d45686d
ilakkiyameenu <30674790+ilakkiyameenu@users.noreply.github.com>
2017-08-19 08:01:40 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,17 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/article-one',function(req,res){
+    res.send('article one is requested and will be served by ilakkiya');
+});
 
+app.get('/article-two',function(req,res){
+    res.send('article two is requested and will be served by ilakkiya');
+});
+
+app.get('/article-three',function(req,res){
+    res.send('article three is requested and will be served by ilakkiya');
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
