6fce1285e42c01f44a6ba02a62fe2838fead384d
ilakkiyameenu <30674790+ilakkiyameenu@users.noreply.github.com>
2017-08-19 10:09:09 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,75 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articles=
+{
+    'article-one':{
+    title:'article one|ilakkiya meenu',
+    heading:'article one!!!',
+    date:' aug 18,2017',
+    content:  `  <p>
+                    this is the content for article one .this is the content for article one 
+                    .this is the content for article one .
+                </p>
+                <p>
+                    this is the content for article one .this is the content for article one 
+                    .this is the content for article one .
+                </p>
+                <p>
+                    this is the content for article one .this is the content for article one 
+                    .this is the content for article one .
+                </p>`
+    },
+    'article-two':{
+    title:'article two|ilakkiya meenu',
+    heading:'article two!!!',
+    date:' aug 15,2017',
+    content:  `  <p>
+                    this is the content for article two.this is the content for article one 
+                    .this is the content for article two .
+                </p>`
+    },
+    'article-three':{
+    title:'article three|ilakkiya meenu',
+    heading:'article three!!!',
+    date:' aug 1,2017',
+    content:  `  <p>
+                    this is the content for article three.this is the content for article three 
+                    .this is the content for article three .
+                </p>`
+    }
+};
+function createTemplate(data)
+{
+var title=data.title;
+var date=data.date;
+var content=data.content;
+var template=`<html>
+    <head>
+        <title>
+        ${title}
+        </title>
+        <link href="ui/style.css" rel="stylesheet"/>
+    </head>
+    
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">home page</a>
+            </div>
+            <hr/>
+            <div>
+                ${date}
+            </div>
+            <h1>article one!!!</h1>
+            <div>
+            ${content}
+            </div>
+        </div>
+    </body>
+</html>`;
+}
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
