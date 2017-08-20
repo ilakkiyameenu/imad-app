8c8f5e7c314f137c3110601521073dc7cdb9e845
ilakkiyameenu <30674790+ilakkiyameenu@users.noreply.github.com>
2017-08-20 16:21:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,14 @@ console.log('Loaded!');
 var element=document.getElementById('main-text'
 );
 element.innerHTML = 'new text';
+var img=document.getElementById('madi');
+var marginleft=0;
+function moveRight()
+{
+marginLeft+=10;
+img.style.marginLeft= marginLeft + 'px';
+}
+img.onclick = function()
+{
+    var interval=setInterval(moveRight,100);
+}
