e90be57b3d581e49453b122be4bf5ae3fd973a59
ilakkiyameenu <30674790+ilakkiyameenu@users.noreply.github.com>
2017-08-20 15:14:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,6 +85,10 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+app.get('/ui/favico.ico', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'favico.ico'));
+});
+
 
 app.get('/:articlename',function(req,res){
 var articlename=req.params.articlename;
