9a1f2118e2230ebd31454a57b16c8028496d9751
ilakkiyameenu <30674790+ilakkiyameenu@users.noreply.github.com>
2017-08-20 16:27:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,13 @@ var element=document.getElementById('main-text'
 );
 element.innerHTML = 'new text';
 var img=document.getElementById('madi');
+var marginleft=0;
+function moveRight()
+{
+marginLeft+=10;
+img.style.marginLeft= marginLeft + 'px';
+}
+
 img.onclick = function()
 {
     img.style.marginLeft =  '100px';
