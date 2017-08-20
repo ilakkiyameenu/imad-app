1876de973d79a6db6feb260888538a6d077407d4
ilakkiyameenu <30674790+ilakkiyameenu@users.noreply.github.com>
2017-08-20 15:43:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,4 @@ console.log('Loaded!');
 //to chnge the text inside the id of main-text
 var element=document.getElementById('main-text'
 );
-element.innerHtml = 'new text';
+element.innerHTML = 'new text';
