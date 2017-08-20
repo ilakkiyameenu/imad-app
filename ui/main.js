8443e4b3f9d4e6fdb62d194d14672d1fa43dd0c1
ilakkiyameenu <30674790+ilakkiyameenu@users.noreply.github.com>
2017-08-20 16:40:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
 //to chnge the text inside the id of main-text
-var element=document.getElementById('ilakkiya anbalagant'
+var element=document.getElementById('main-text'
 );
-element.innerHTML = 'new text';
+element.innerHTML = 'ilakkiya anbalagan';
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight()
