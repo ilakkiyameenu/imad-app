af3a3b2cced19e1c3985e4356a944a79b81a9ece
ilakkiyameenu <30674790+ilakkiyameenu@users.noreply.github.com>
2017-08-25 16:35:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ button.onclick=function (){
     request.open('GET','http://ilakkiyameenu1997.imad.hasura-app.io/counter',true);
     request.send(null);
 };
-var submit=gtElementById('submit_btn');
+var submit=getElementById('submit_btn');
     submit.onclick=function(){
       //make arequest to the server and send the name
       //capture a list of names and render it
