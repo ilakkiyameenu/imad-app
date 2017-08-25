dc45d4b28c50a89701661801ef5f92b53d328e5e
ilakkiyameenu <30674790+ilakkiyameenu@users.noreply.github.com>
2017-08-25 08:16:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,13 @@
-console.log('Loaded!');
-//to chnge the text inside the id of main-text
-var element=document.getElementById('main-text');
-element.innerHTML = 'ilakkiya anbalagan';
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight()
-{
-marginLeft=marginLeft+1;
-img.style.marginLeft= marginLeft + 'px';
-}
-
-img.onclick = function()
-{
-var interval= setInterval(moveRight,10);
+//counter code
+var button=document.getElementById(counter);
+var counter=0;
+button.onclick=function (){
+    //make a request to the counter endpoint
+    
+    //get the response and store it in a varible
+    
+    //render the variable in the correct span
+    counter=counter+1;
+    var span=document.getElementById(count);
+    span.innerHTML=counter.toString();
 };
