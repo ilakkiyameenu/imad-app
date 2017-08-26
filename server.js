c61ec0d42915fcd6acc227dd042e7cbe056e64f1
ilakkiyameenu <30674790+ilakkiyameenu@users.noreply.github.com>
2017-08-26 07:43:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,14 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/favicon.ico', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'favico.ico'));
 });
+app.get('/submit-name/:name',function(req,res){
+    //extract name from the request
+var name=req.params.name;
+names.push(name);
+//JSON javascript object notation
+res.send(JSON.stringify(names));
+
+});
 var counter =0;
 app.get('/counter',function(req,res){
     counter=counter+1;
