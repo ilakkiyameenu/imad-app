40f2b66957dc46ef4f7c7f8b3fc2e8299c5d6c70
ilakkiyameenu <30674790+ilakkiyameenu@users.noreply.github.com>
2017-08-26 07:49:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,7 @@ submit.onclick=function(){
       var names=['ilakkiya','meenu','3rdname'];
       var list='';
       for( var i=0;i<names.length;i++){
-          list=='<li>'+names[i]+'</li>';
+          list+='<li>'+names[i]+'</li>';
       }
       var ul=document.getElementById('unordered');
       ul.innerHTML=list;
