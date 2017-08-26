8c7d2412df7d31e905a03c199e5450105d934df9
ilakkiyameenu <30674790+ilakkiyameenu@users.noreply.github.com>
2017-08-26 08:36:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,17 +21,33 @@ button.onclick=function (){
     request.open('GET','http://ilakkiyameenu1997.imad.hasura-app.io/counter',true);
     request.send(null);
 };
-var nameInput=document.getElementById('name');
-var namev=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
       //make arequest to the server and send the name
+      var request= new XMLHttpRequest();
+    
+    //get the response and store it in a varible
+    request.onreadystatechange =function(){
+      if(request.readyState===XMLHttpRequest.DONE)
+      //take some action
+      if(request.status===200)
+    {
       //capture a list of names and render it
-      var names=['ilakkiya','meenu','3rdname'];
+      var names=request.responseText;
       var list='';
       for( var i=0;i<names.length;i++){
           list+='<li>'+names[i]+'</li>';
       }
       var ul=document.getElementById('unordered');
       ul.innerHTML=list;
+
+      }
     };
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+    //make a request
+    
+    request.open('GET','http://ilakkiyameenu1997.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+       };
