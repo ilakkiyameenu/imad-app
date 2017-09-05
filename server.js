1bea9215b7760c5c794bdc329422373ac7389af6
ilakkiyameenu <30674790+ilakkiyameenu@users.noreply.github.com>
2017-09-05 08:05:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,13 +132,14 @@ pool.query("SELECT*FROM articles WHERE title=' "+req.params.articlename+"'",func
     if(err){
         res.status(500).send(err.toString());
     }
-    else(result.rows.length===0)
+    else 
+    if(result.rows.length===0)
     {
         res.status(404).send('row not found');
     }
     else{
         var articledata=result.rows[0];
- res.send(createTemplate(articledata));
+        res.send(createTemplate(articledata));
     }
     
 });
