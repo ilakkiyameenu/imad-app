5d7e6be28090fd116510d0b7e357f8284126c858
ilakkiyameenu <30674790+ilakkiyameenu@users.noreply.github.com>
2017-09-05 08:03:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,9 +126,23 @@ app.get('/counter',function(req,res){
 });
 
 
-app.get('/:articlename',function(req,res){
-var articlename=req.params.articlename;
- res.send(createTemplate(articles[articlename]));
+app.get('/articles/:articlename',function(req,res){
+pool.query("SELECT*FROM articles WHERE title=' "+req.params.articlename+"'",function(err,result)
+{
+    if(err){
+        res.status(500).send(err.toString());
+    }
+    else(result.rows.length===0)
+    {
+        res.status(404).send('row not found');
+    }
+    else{
+        var articledata=result.rows[0];
+ res.send(createTemplate(articledata));
+    }
+    
+});
+
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
