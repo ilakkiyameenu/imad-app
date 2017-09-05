b49ce8d9d9aae94db72c501d6c87090dce27f7e8
ilakkiyameenu <30674790+ilakkiyameenu@users.noreply.github.com>
2017-09-05 06:53:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config={
+    user:'ilakkiyameenu1997',
+    database:'ilakkiyameenu1997',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -81,6 +90,19 @@ app.get('/', function (req, res) {
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+   //make a select request
+   pool.query('SELECT * FROM test',function(err,res){
+       if(err){
+           res.status(500).send(err.toString())
+       }else
+       {
+           res.send(JSON.stringify(result));
+       }
+   })
+   //return the response
 });
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
